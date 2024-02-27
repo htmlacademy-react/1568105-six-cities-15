59e84cd114f0027dc38a3bbd356d56020d7375ad
aManAndCode <105123221+aManAndCode@users.noreply.github.com>
2024-02-27 18:42:42 +0300

    Правки в коде

--- src/pages/offer/offer.tsx
+++ src/pages/offer/offer.tsx
@@ -170,13 +170,7 @@ export default function Offer() {
               <article className="near-places__card place-card">
                 <div className="near-places__image-wrapper place-card__image-wrapper">
                   <a href="#">
-                    <img
-                      className="place-card__image"
-                      src="img/room.jpg"
-                      width={260}
-                      height={200}
-                      alt="Place image"
-                    />
+                    <img className="place-card__image" src="img/room.jpg" width={260} height={200} alt="Place image" />
                   </a>
                 </div>
                 <div className="place-card__info">
@@ -185,15 +179,8 @@ export default function Offer() {
                       <b className="place-card__price-value">&euro;80</b>
                       <span className="place-card__price-text">&#47;&nbsp;night</span>
                     </div>
-                    <button
-                      className="place-card__bookmark-button place-card__bookmark-button--active button"
-                      type="button"
-                    >
-                      <svg
-                        className="place-card__bookmark-icon"
-                        width={18}
-                        height={19}
-                      >
+                    <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button" >
+                      <svg className="place-card__bookmark-icon" width={18} height={19} >
                         <use xlinkHref="#icon-bookmark" />
                       </svg>
                       <span className="visually-hidden">In bookmarks</span>
@@ -257,10 +244,7 @@ export default function Offer() {
                       <b className="place-card__price-value">&euro;180</b>
                       <span className="place-card__price-text">&#47;&nbsp;night</span>
                     </div>
-                    <button
-                      className="place-card__bookmark-button button"
-                      type="button"
-                    >
+                    <button className="place-card__bookmark-button button" type="button" >
                       <svg className="place-card__bookmark-icon" width={18} height={19} >
                         <use xlinkHref="#icon-bookmark" />
                       </svg>
